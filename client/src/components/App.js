693e9fcfe652ec13ef0d02a11484009b0b2fc2db
Troy <troyakins99@yahoo.com>
2021-09-14 09:42:01 -0500

    front-end functionality added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import '../App.css';
 import Login from '../Login';
-import NavBar from './Navbar';
+import NavBar from '../Navbar';
 import Main from '../ImageContainer';
 
 function App() {
-  const [user, setUser] = useState(null);
+  
 
-  useEffect(() => {
-    // auto-login
-    fetch("/me").then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user));
-      }
-    });
-  }, []);
 
-  // if (!user) return <Login onLogin={setUser} />;
 
   return (
 
@@ -26,7 +17,6 @@ function App() {
 
       <NavBar/>
       <Login/>
-        
       </div>
       {/* <NavBar user={user} setUser={setUser} /> */}
       {/* <main>
